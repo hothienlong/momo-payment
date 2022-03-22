a81a1a341afa8b4b3bb5acaf1fc170330bb25b1d
Long Hồ <thienlong460@gmail.com>
2022-03-23 00:00:36 +0700

    + add Momo service

--- src/controllers/momo/createOrderController.js
+++ src/controllers/momo/createOrderController.js
@@ -1,63 +1,20 @@
-import { v4 as uuidv4 } from 'uuid';
-import Crypto from '../../momo/crypto';
-import account from '../../util/account';
-import sendRequest from '../../util/api';
-import config from '../../../config.json';
+import Momo from '../../momo/momo';
 
 export default async (req, res) => {
 	console.log('createOrderController');
 
-	var requestId = uuidv4();
-	var orderId = uuidv4();
-	var orderInfo = 'Thanh toán cho đơn hàng\n' + orderId;
 	var redirectUrl = req.body.redirectUrl;
 	var ipnUrl = req.body.ipnUrl;
-	// var ipnUrl = redirectUrl = "https://webhook.site/454e7b77-f177-4ece-8236-ddf1c26ba7f8";
 	var amount = req.body.amount;
-	var requestType = 'captureWallet';
-	var extraData = ''; //pass empty value if your merchant does not have stores
 
-	console.log('--------------------SIGNATURE----------------');
+	const { res: response, message } = await Momo.getInstance().createOrder(
+		redirectUrl,
+		ipnUrl,
+		amount
+	);
 
-	var dataSignature = {
-		accessKey: account.accessKey,
-		amount: amount,
-		extraData: extraData,
-		ipnUrl: ipnUrl,
-		orderId: orderId,
-		orderInfo: orderInfo,
-		partnerCode: account.partnerCode,
-		redirectUrl: redirectUrl,
-		requestId: requestId,
-		requestType: requestType,
-	};
-
-	const signature = Crypto.getInstance().compute(dataSignature);
-	console.log(signature);
-
-	//json object send to MoMo endpoint
-
-	const data = JSON.stringify({
-		partnerCode: account.partnerCode,
-		partnerName: account.partnerName,
-		requestId: requestId,
-		amount: amount,
-		orderId: orderId,
-		orderInfo: orderInfo,
-		redirectUrl: redirectUrl,
-		ipnUrl: ipnUrl,
-		extraData: extraData,
-		requestType: requestType,
-		signature: signature,
-		lang: 'vi',
-	});
-
-	//console.log(data);
-
-	const { res: response, message } = await sendRequest('POST', {
-		url: config.api.createOrder,
-		data: data,
-	});
+	console.log(response);
+	console.log(message);
 
 	if (!response) return res.status(500).json(message);
 	return res.status(200).json(response);
--- src/momo/momo.js
+++ src/momo/momo.js
@@ -1,8 +1,66 @@
+import { v4 as uuidv4 } from 'uuid';
+import sendRequest from '../util/api';
+import config from '../../config.json';
+import Crypto from './crypto';
+
 class Momo {
-    constructor() {
-      }
+	static #instance = null;
 
-    initialPaymentMethod(data) {
+	static getInstance() {
+		if (this.#instance === null) {
+			return new Momo();
+		}
 
-    }
+		console.log('New Momo');
+
+		return this.#instance;
+	}
+
+	getNewOrder(redirectUrl, ipnUrl, amount) {
+		var requestId = uuidv4();
+		var orderId = uuidv4();
+		var orderInfo = 'Thanh toán cho đơn hàng\n' + orderId;
+
+		var requestType = 'captureWallet';
+		var extraData = ''; //pass empty value if your merchant does not have stores
+
+		const order = {
+			amount: amount,
+			extraData: extraData,
+			ipnUrl: ipnUrl,
+			orderId: orderId,
+			orderInfo: orderInfo,
+			partnerCode: config.partnerCode,
+			redirectUrl: redirectUrl,
+			requestId: requestId,
+			requestType: requestType,
+		};
+		const dataSignature = {
+			accessKey: config.accessKey,
+			...order,
+		};
+
+		const signature = Crypto.getInstance().compute(dataSignature);
+
+		return {
+			partnerName: config.partnerName,
+			signature: signature,
+			lang: 'vi',
+			...order,
+		};
+	}
+
+	async createOrder(redirectUrl, ipnUrl, amount) {
+		console.log('create order');
+		const order = this.getNewOrder(redirectUrl, ipnUrl, amount);
+
+		const { res, message } = await sendRequest('POST', {
+			url: config.api.createOrder,
+			data: JSON.stringify(order),
+		});
+
+		return { res, message };
+	}
 }
+
+export default Momo;
