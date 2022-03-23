54e5e5915050a5a168bbe5b7bea8918bc196109d
Long Hồ <thienlong460@gmail.com>
2022-03-23 16:06:16 +0700

    - remove user model

--- src/router.js
+++ src/router.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import momoRouter from './routes/momo';
-import userRouter from './routes/users';
 
 const router = express.Router();
 
-router.use('/users', userRouter);
 router.use('/momo', momoRouter);
 
 export default router;
