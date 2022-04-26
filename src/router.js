45ed76d0645f2001096b0129568750559b5b2720
Long Hồ <thienlong460@gmail.com>
2022-04-26 21:26:43 +0700

    + add test route

--- src/router.js
+++ src/router.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import momoRouter from './routes/momo';
 import paypalRouter from './routes/paypal';
+import testRouter from './routes/test';
 
 const router = express.Router();
 
 router.use('/paypal', paypalRouter);
 router.use('/momo', momoRouter);
-router.use('/', momoRouter);
+router.use('/', testRouter);
 
 export default router;
